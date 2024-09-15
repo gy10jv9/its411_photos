8e136a407929a9d92e27c0e99506baae7f378463
gy10jv9 <146041946+gy10jv9@users.noreply.github.com>
2024-09-15 22:58:46 +0800

    added safe area view

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+        <Text>Open up App.tsx to start working on your app!! </Text>
+    </SafeAreaView>
+  );
+}
